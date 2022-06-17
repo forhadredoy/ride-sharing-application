a8e874a8629044fb59d23fda4afca6d1b8a8fa2d
forhad hossain redoy <forhadhridoy.43@gmail.com>
2022-06-18 01:27:09 +0600

    second commit

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ var animateButton = function(e) {
   var bubblyButtons = document.getElementsByClassName("bubbly-button");
   
   for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
+    bubblyButtons[i].addEventListener('mouseover', animateButton, false);
   }
+  //input
+    // jquery transit is used to handle the animation
+    $('input').focusin(function() {
+      $('label').transition({x:'80px'},500,'ease').next()
+      .transition({x:'5px'},500, 'ease');
+//setTimeout needed for Chrome, for some reson there is no animation from left to right, the pen is immediately present. Slight delay to adding the animation class fixes it
+       setTimeout(function(){
+      $('label').next().addClass('move-pen');
+      },100);
+  
+  });
+    
+    $('input').focusout(function() {
+        $('label').transition({x:'0px'},500,'ease').next()
+         .transition({x:'-100px'},500, 'ease').removeClass('move-pen');
+    });
